7e6c559f7475f49fe76cd15d32043a1ae16922fa
sergeycher <sergeycher@yandex.ru>
2023-12-02 00:22:54 +0300

    refactor(mmo): travelling locations

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 export {Trait} from "./src/traits/trait";
+export {Traits} from "./src/traits/traits";
 export {Unit} from './src/units/unit';
 export {Tie} from './src/ties/tie';
 export {Ties} from "./src/ties/ties.trait";
